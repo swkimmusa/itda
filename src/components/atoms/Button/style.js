93f39d47cfea1f67cee316fcc641cc9d14f80456
eugene choi <eugene.maestro.choi@gmail.com>
2023-02-18 17:55:49 +0900

    minor style update button

--- src/components/atoms/Button/style.js
+++ src/components/atoms/Button/style.js
@@ -1,8 +1,12 @@
 import { css } from 'styled-components';
 
 export const unsetStyle = css`
+  display: flex;
   text-decoration: none;
   appearance: none;
+  cursor: pointer;
+  border: none;
+  background: none;
 
   &:focus {
     outline: none;
