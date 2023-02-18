333a8e493c7829ab453e6f8071d4e4f257cd694c
eugene choi <eugene.maestro.choi@gmail.com>
2023-02-18 16:41:12 +0900

    PageAction repositions on mobile against left menu

--- src/components/organisms/PageAction/index.js
+++ src/components/organisms/PageAction/index.js
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+
 import startCase from 'lodash/startCase';
-import { palette } from 'styled-theme';
+import {
+  palette, size,
+} from 'styled-theme';
 import { ifProp } from 'styled-tools';
 
 import Button from '../../atoms/Button';
@@ -14,6 +17,9 @@ const Wrapper = styled(Flex)`
   right: 15px;
   flex-direction: column;
   background-color: transparent;
+  @media (min-width: ${size('mobileBreakpoint')}){
+    left: calc(250px + 15px);
+  }
 `;
 
 const PageAction = ({
