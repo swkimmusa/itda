07e855230ae06e8f2d3a9b6f62a28210a3835dc2
eugene choi <eugene.maestro.choi@gmail.com>
2023-02-22 17:17:37 +0900

    bublewrap deploy

---
+++ app/src/main/java/kr/co/itdaapp/staging/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package kr.co.itdaapp.staging.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
